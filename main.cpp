3602584de2f53dd912f3ed30a27220089ecc4224
Helmi <rumahhelmi@gmail.com>
2025-06-24 13:55:17 +0800

    Membuat struktur data untuk tema antrian

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <map>
+
+
+enum Status { PENDING, PROSES, SELESAI };
+std::map<Status, std::string> statusMap{
+{PENDING, "Pending"},
+{PROSES, "Proses"},
+{SELESAI, "Selesai"},
+};
+
+
+// Struct data
+struct Node{
+
+    int id;
+    std::string name;
+    int phoneNum;
+    std::string desc;
+    std::time_t time;
+    Status status;
+    Node *next;
+
+    Node(): time(std::time(NULL)), status(PENDING), next(nullptr){};
+};
+
+// Struct Queue menggunakan linkedlist
+struct Queue{
+    Node *head;
+    Node *tail;
+
+    // Constructor untuk mendefinisikan default head dan tail;
+    Queue(): head(nullptr), tail(nullptr){};
+    // Destructor untuk handle memory safety
+    ~Queue(){
+        while(head != nullptr)
+        {
+            Node *tmp = head;
+            head = head->next;
+            delete tmp;
+        }
+        tail = nullptr;
+        std::cout << "Success handle memory safety" << std::endl;
+    }
+
+    // Mendefinisasikan fungsi dari Queue
+    // Note: fungsi yang bukan original dari Queue didefinisasikan dengan tanda "_"
+    bool _isNull();
+    void _insert(const size_t index, const std::string &name, const int phoneNum, const std::string &desc);
+    void _remove(size_t index);
+    void pop();
+    void push(const std::string &name, const int phoneNum, const std::string &desc);
+    // void front();
+    // void back();
+    void show();
+    void updateStatus(size_t index, Status newStatus);
+    void showBasedOnStatus(Status status);
+    void showmMenu();
+    size_t size();
+};
+
